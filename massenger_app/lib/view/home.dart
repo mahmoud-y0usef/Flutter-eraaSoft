d001477a7498af541c1bbf07a04183c7f918ae8b
mahmoudYousef <mahmoudyousef59@outlook.com>
2025-02-25 01:02:30 +0200

    update

--- massenger_app/lib/view/home.dart
+++ massenger_app/lib/view/home.dart
@@ -1,10 +1,108 @@
-import 'package:massenger_app/core/colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:massenger_app/core/route.dart';
+import 'package:massenger_app/core/colors.dart';
 import 'package:massenger_app/widgits/appbar.dart';
-import 'package:massenger_app/widgits/container.dart';
 import 'package:massenger_app/widgits/nav.dart';
+
+final List<Map<String, dynamic>> chatList = [
+  {
+    "name": "Warren Buffett",
+    "message": "Hanif can you marry my Daughter..",
+    "time": "10:20 pm",
+    "avatar": "assets/images/warren_buffett.jpg",
+    "isOnline": true
+  },
+  {
+    "name": "Mark Zuckerberg",
+    "message": "Asraful are you available for...",
+    "time": "11:10 pm",
+    "avatar": "assets/images/mark_zuckerberg.jpg",
+    "isOnline": true
+  },
+  {
+    "name": "William Henry Gates",
+    "message": "Please reply to me Asraful...",
+    "time": "12:00 am",
+    "avatar": "assets/images/bill_gates.jpg",
+    "isOnline": true
+  },
+  {
+    "name": "Vladimir Putin",
+    "message": "Hey Putin don't disturb me, I am bu...",
+    "time": "12:00 am",
+    "avatar": "assets/images/putin.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Jeff Bezos",
+    "message": "My daughter is crying for you hanif...",
+    "time": "09:00 am",
+    "avatar": "assets/images/jeff_bezos.jpg",
+    "isOnline": true
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+  {
+    "name": "Barack Obama",
+    "message": "I want to meet with you hanif ...",
+    "time": "11:00 am",
+    "avatar": "assets/images/obama.jpg",
+    "isOnline": false
+  },
+];
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,40 +115,88 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorsApp.tertiary,
+      backgroundColor: Colors.grey[200],
       appBar: AppBarCustom(),
       bottomNavigationBar: Nav(),
-      body: Column(
-        children: <Widget>[
-          const SizedBox(height: 16),
-          Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: ColorsApp.secondary,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(24),
-                  bottomRight: Radius.circular(24),
-                ),
-              ),
 
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.search,
-                    color: ColorsApp.primary,
-                  ),
-                  const SizedBox(width: 16),
-                  Text(
-                    'Search',
-                    style: TextStyle(
-                      color: ColorsApp.primary,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+      body: Column(
+        children: [
+
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 1,
+                    blurRadius: 5,
                   ),
                 ],
-              )
-          )
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 15),
+                ),
+              ),
+            ),
+          ),
+
+          // 📩 قائمة الدردشات
+          Expanded(
+            child: ListView.builder(
+              itemCount: chatList.length,
+              itemBuilder: (context, index) {
+                final chat = chatList[index];
+                return ListTile(
+                  leading: Stack(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage(chat["avatar"]),
+                        radius: 24,
+                      ),
+                      if (chat["isOnline"])
+                        Positioned(
+                          bottom: 2,
+                          right: 2,
+                          child: Container(
+                            width: 12,
+                            height: 12,
+                            decoration: BoxDecoration(
+                              color: Colors.green,
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.white, width: 2),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                  title: Text(
+                    chat["name"],
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    chat["message"],
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  trailing: Text(
+                    chat["time"],
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                  onTap: () {
+                    // 👇 عند الضغط على المحادثة، انتقل إلى صفحة المحادثة
+                    // Navigator.pushNamed(context, RouteName.chatScreen, arguments: chat);
+                  },
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
